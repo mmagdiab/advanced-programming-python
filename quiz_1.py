a5b94ae90321b2dc4f2b3773c289e7cb2661d035
Mohamed Magdy <14momagdi@gmail.com>
2023-01-02 13:49:10 +0200

    add quiz_1, quiz_2

---
+++ quiz_1.py
@@ -0,0 +1,38 @@
+"""
+Your Program will read a list of questions,
+a list of the corresponding correct answers, in addition to a list of wrong answers.
+The program will then compose an MCQ exam paper of 5 random questions.
+Each question in the exam paper will be associated with 4 choices. One of the choices will be its correct answer,
+while the other choices will be wrong answer.
+Your program will also read student responses and will grade them.
+Each correct answer will count one mark and each wrong answer will count zero marks.
+You are kindly required to write Python code to realize this idea.
+The program is supposed to read response of 100 students and produce the exam results.
+"""
+from random import sample, shuffle
+
+# MCQ creation
+questions_bank = eval(input('Enter questions list:'))
+answers_bank = eval(input('Enter corresponding answers list:'))
+wrong_bank = eval(input('Enter wrong choices list:'))
+
+random_question = sample(questions_bank, 5)
+
+mcq_exam = [{  # each question -> {'question': value, 'answer': value, 'choices': value}
+    'question': question,
+    'answer': answers_bank[questions_bank.index(question)],
+    'choices': sample(wrong_bank, 3) + [answers_bank[questions_bank.index(question)]]
+} for question in random_question]
+
+# student questioning
+for i in range(100):
+    student_score = 0
+    shuffle(mcq_exam)
+    for mcq in mcq_exam:
+        print('Question: ' + mcq['question'])
+        shuffle(mcq['choices'])
+        print('Choices: ' + " - ".join(mcq['choices']))
+        answer = input('Enter your answer:')
+        if answer.lower() == mcq['answer'].lower():
+            student_score += 1
+    print(student_score)
---
+++ quiz_2.py
@@ -0,0 +1,61 @@
+"""
+Write a Python program that uses a class called DATE,
+a class called TodayDate, and a class called Person BirthDate.
+The class DATE would be the parent class for the other two classes and
+should include two fields namely day, month, and year. The user will be allowed to enter the date of today and
+his birth_date, and the program would use a function called Calculate Age to calculate and print the age of the user
+in a suitable format.
+"""
+
+
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+
+class TodayDate(Date):
+    def __int__(self, year, month, day):
+        super().__init__(year, month, day)
+
+
+class BirthDate(Date):
+    def __int__(self, year, month, day):
+        super().__init__(year, month, day)
+
+
+def calculate_age(today_date, birth_date):
+    years_alive = today_date.year - birth_date.year
+    months_alive = today_date.month - birth_date.month
+    days_alive = today_date.month - birth_date.month
+
+    if days_alive < 0:
+        if months_alive > 0:  # Break 1 month into days
+            months_alive -= 1
+            days_alive += 30
+        elif years_alive > 0:  # if no months, break a year into 11 month and 30 days
+            months_alive += 11
+            days_alive += 30
+        else:  # if no years available the date in future
+            print('The entered date is invalid')
+            return
+
+    if months_alive < 0:
+        if years_alive > 0:  # break a year into 12 month
+            months_alive += 12
+            years_alive -= 1
+        else:  # if no years available the date in future
+            print('The entered date is invalid')
+            return
+
+    print('You are alive for {} years, {} months and {} days'.format(years_alive, months_alive, days_alive))
+
+
+today = input('Enter today\'s date in the format dd-mm-yyyy: ')
+birth = input('Enter birth date in the format dd-mm-yyyy: ')
+
+today = TodayDate(int(today[6:]), int(today[3:5]), int(today[:2]))
+birth = BirthDate(int(birth[6:]), int(birth[3:5]), int(birth[:2]))
+
+calculate_age(today, birth)
